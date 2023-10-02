23aa75632d7e78ed5d1cb0f3398a191bd066543e
georgiana <a43190_cpa@visma.com>
2023-10-02 16:42:04 +0300

    test2

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -27,6 +27,6 @@ resource "google_compute_firewall" "allow-http" {
     ports    = ["80"]
   }
 
-  target_tags   = ["http-server", "test"]
+  target_tags   = ["http-server", "test", "test2"]
   source_ranges = ["0.0.0.0/0"]
 }
